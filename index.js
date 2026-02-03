951cf3e65798ae7ca38d1b45c3594c84086eee2f
Himanshu Yadav <himanshu.yadav@jaspercolin.com>
2026-02-03 17:51:29 +0530

    changes

--- index.js
+++ index.js
@@ -134,27 +134,25 @@
     };
   }
 
-async function collectTimeLocale() {
-  const date = new Date();
+  async function collectTimeLocale() {
+    const date = new Date();
 
-  return {
-    time: {
-      nowISO: date.toISOString(),
-      epochMs: date.getTime(),
-      timezoneOffsetMinutes: date.getTimezoneOffset(),
-      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone || null,
-    },
-    
-    locale: {
-      primary: navigator.language || null,
-      calendar: Intl.DateTimeFormat().resolvedOptions().calendar || null,
-      numberingSystem: Intl.NumberFormat().resolvedOptions().numberingSystem || null
-    }
-  };
-}
+    return {
+      time: {
+        nowISO: date.toISOString(),
+        epochMs: date.getTime(),
+        timezoneOffsetMinutes: date.getTimezoneOffset(),
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone || null,
+      },
 
-
-
+      locale: {
+        primary: navigator.language || null,
+        calendar: Intl.DateTimeFormat().resolvedOptions().calendar || null,
+        numberingSystem:
+          Intl.NumberFormat().resolvedOptions().numberingSystem || null,
+      },
+    };
+  }
 
   async function collectAll(trigger = "manual") {
     const data = {
